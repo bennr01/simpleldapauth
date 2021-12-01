3a584655d9f5c563b3214bfae77ed66996192ef1
bennr01 <benjamin99.vogt@web.de>
2021-12-01 22:26:21 +0100

    improve test formulation

--- simpleldapauth/__init__.py
+++ simpleldapauth/__init__.py
@@ -1,5 +1,5 @@
 """
 This package contains the LDAP auth logic.
 
-This is mostly taken from kadi4mat by the Karlsruhe Institute of Technology.
+The code in this package  is mostly taken from kadi4mat by the Karlsruhe Institute of Technology.
 """
